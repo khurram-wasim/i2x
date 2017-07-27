6d10fa62cfbd547dc0d35a40e34dffd2f4db3b94
Khurram Wasim <khurram0403@gmail.com>
2017-07-27 09:03:03 +0500

    minor change.

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const {resolve} = require('path');
 const webpack = require('webpack');
 const validate = require('webpack-validator');
 const {getIfUtils, removeEmpty} = require('webpack-config-utils');
+const argv = require('minimist')(process.argv.slice(2));
 
 module.exports = env => {
   const {ifProd, ifNotProd} = getIfUtils(env)
   const activePort = process.env.PORT || 8080;
   console.log('HOST IS ',process.env.HOST);
+  console.log('HOST2 IS ',argv.host);
   return validate({
     entry: './index.js',
     context: __dirname,
